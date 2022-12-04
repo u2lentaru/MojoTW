f07ff0fe5e1c8d5fc374586181e2c337d7675d95
u2 <u2@lenta.ru>
2022-12-04 21:16:59 +0500

    saveadd hbm

--- lib/TestWork/Controller/List.pm
+++ lib/TestWork/Controller/List.pm
@@ -31,12 +31,22 @@ sub saveadd ($self) {
     my $ts = localtime(time);
     
     my $ua  = Mojo::UserAgent->new;
-    my $res = $ua->get($newurl)->result;
+    my $res_ora = $ua->get($newurl);
+    my ($hst, $hhl, $hhc, $hhs) = (" ", " ", " ", " ");
 
-    my $hst = $res->{code}." ".$res->{message};
-    my $hhl = $res->headers->location;
-    my $hhc =  $res->headers->content_type;
-    my $hhs =  $res->headers->server;
+    if (defined ($res_ora->original_remote_address)) {
+      my $res = $ua->get($newurl)->result;
+      $hst = $res->{code}." ".$res->{message};
+      $hhl = $res->headers->location;
+      $hhc =  $res->headers->content_type;
+      $hhs =  $res->headers->server;
+    }
+    else {
+      $hst = "Cannot resolve host";
+      $hhl = "Cannot resolve host";
+      $hhc = "Cannot resolve host";
+      $hhs = "Cannot resolve host";
+    };
 
     $self->pg->db->query('insert into url_list (url, urldate, httpstatus, httphead1, httphead2, httphead3) values (?, ?, ?, ?, ?, ?)', $newurl, $ts, $hst, $hhl, $hhc, $hhs);
     $self->redirect_to("/");   
@@ -72,10 +82,10 @@ sub saveupd ($self) {
       $hhs =  $res->headers->server;
     }
     else {
-      $hst = "Error";
-      $hhl = "Error";
-      $hhc = "Error";
-      $hhs = "Error";
+      $hst = "Cannot resolve host";
+      $hhl = "Cannot resolve host";
+      $hhc = "Cannot resolve host";
+      $hhs = "Cannot resolve host";
     };
 
     $self->pg->db->query('update url_list set (url, urldate, httpstatus, httphead1, httphead2, httphead3) = (?,?,?,?,?,?) where id = ?', $newurl, $ts, $hst, $hhl, $hhc, $hhs, $id);
