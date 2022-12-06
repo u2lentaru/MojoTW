3aaecfa2f843c64a4d9f59397a4856469f0076bc
u2 <u2@lenta.ru>
2022-12-06 16:18:17 +0500

    CRUD hbm

--- lib/TestWork.pm
+++ lib/TestWork.pm
@@ -15,8 +15,7 @@ sub startup ($self) {
   $self->secrets($config->{secrets});
 
   $self->helper(pg => sub { state $pg = Mojo::Pg->new('postgres://postgres:postgres@localhost:5432/postgres') });
-  # $self->pg->db->query('select version()');
-
+  
   $self->pg->db->query('CREATE TABLE IF NOT EXISTS public.url_list (
 	id int NOT NULL GENERATED ALWAYS AS IDENTITY,
 	url text NOT NULL,
@@ -32,7 +31,6 @@ sub startup ($self) {
   my $r = $self->routes;
 
   # Normal route to controller
-  # $r->get('/')->to('example#welcome');
   $r->get('/')->to('list#list');
   $r->get('/list/')->to('list#list');
   $r->get('/add/')->to('list#showadd');
@@ -41,108 +39,6 @@ sub startup ($self) {
   $r->get('/upd/')->to('list#showupd');
   $r->post('/upd/')->to('list#saveupd');
   $r->get('/poll/')->to('list#poll');
-
-# Perform operation in 30 seconds
-# Mojo::IOLoop->timer(30 => sub ($self->redirect_to("/poll/"));
-
-# 1
-# use strict;
-# use warnings 'all';
-# use feature 'say';
-
-# use Mojo;
-
-# use constant URL => 'http://stackoverflow.com/questions/tagged/perl';
-
-# STDOUT->autoflush;
-
-# my $ua = Mojo::UserAgent->new;
-# my $delay = Mojo::IOLoop::Delay->new;
-
-# my @urls = ( URL ) x 10;
-
-# my @steps =  map {
-
-#     my $url = $_;
-
-#     sub {
-
-#         my $end = $delay->begin;
-
-#         $ua->get( $url => sub {
-#             my ( $ua, $txn ) = @_;
-
-#             $end->();
-
-#             if ( my $err = $txn->error ) {
-#                 say $err->message;
-#             }
-#             else {
-#                 my $res = $txn->success;
-#                 say "@{$res}{qw/ code message /}";
-#             }
-#         });
-#     }
-
-# } @urls;
-
-
-# $delay->steps( @steps, sub { say 'Done' } );
-
-# Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
-
-
-
-
-
-
-
-# 2
-# #!/usr/bin/perl
-# use Mojo::Base -strict;
-
-# use Mojo::IOLoop;
-# use Mojo::UserAgent;
-
-# my $ua = Mojo::UserAgent->new;
-
-# Mojo::IOLoop->delay(sub {
-#   my $delay = shift;
-#   $ua->get('https://www.google.ru' => $delay->begin);
-# }, sub {
-#   my ($delay, $tx) = @_;
-#   $tx->res->body; # body of the google.ru
-#   $ua->get('https://twitter.com' => $delay->begin);
-# }, sub {
-#   my ($delay, $tx) = @_;
-#   $tx->res->body; # body of the twitter.com
-#   warn 'DONE';
-# })->wait;
-
-################Mojo::Server::Daemon#######################
-
-# use Mojo::UserAgent;
-# use Mojo::Server::Daemon;
-
-# my $ua = Mojo::UserAgent->new;
-# my $daemon = Mojo::Server::Daemon->new(
-#   listen => ['https://*:443']
-# )->unsubscribe('request');
-# $daemon->on(request => sub {
-#   my ($daemon, $tx) = @_;
-
-#   my $req = $tx->req->clone;
-#   $req->url->scheme("https")->host("10.3.199.40");
-
-#   $ua->start(Mojo::Transaction::HTTP->new(req => $req) => sub {
-#     my ($ua, $proxy_tx) = @_;
-#     $tx->res($proxy_tx->res)->resume;
-#   });
-# });
-# $daemon->run;
- 
-
-#######################################
 
 }
 
--- lib/TestWork/Controller/List.pm
+++ lib/TestWork/Controller/List.pm
@@ -1,26 +1,16 @@
 package TestWork::Controller::List;
-# use warnings;
+use warnings;
 use strict;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Mojo::Pg;
-use LWP::Simple;
 use Mojo::UserAgent;
-use Data::Dumper;
-use Mojo::IOLoop::Delay;
-use Mojo::Transaction;
 
 our $urlid = 0;
 our $urlname = "";
 
 sub list ($self) {
-  # foreach my $rec (@{$pg->db->query('select * from url_list')->hashes->to_array}) {
-  #   $tbl .="<tr><td>".$rec->{id}."</td>"."<td>".$rec->{url}."</td>"."<td>".$rec->{urldate}."</td>"."<td>".$rec->{httpstatus}.
-  #   "</td>"."<td>".$rec->{httphead1}."</td>"."<td>".$rec->{httphead2}."</td>"."<td>".$rec->{httphead3}."</td></tr>"
-  # }
-   
   $self->render(template => 'list/list');
 }
-
 
 sub showadd ($self) {
   $self->render(template => 'list/add');
@@ -73,7 +63,6 @@ sub saveupd ($self) {
     my $res_ora = $ua->get($newurl);
     my ($hst, $hhl, $hhc, $hhs) = (" ", " ", " ", " ");
 
-    # say "original_remote_address ",($res_ora->original_remote_address);
     if (defined ($res_ora->original_remote_address)) {
       my $res = $ua->get($newurl)->result;
       $hst = $res->{code}." ".$res->{message};
@@ -95,29 +84,34 @@ sub saveupd ($self) {
 sub poll ($self) {
   my $ua  = Mojo::UserAgent->new;
  
-  foreach my $rec (@{$self->pg->db->query('select id, url from url_list')->hashes->to_array}) {
-    my $uaurl = $rec->{url};
-    say "uaurl ", $uaurl;
-    my $uaurlid = $rec->{id};
-    # say "uaurlid ", $uaurlid, "\n";
+    foreach my $rec (@{$self->pg->db->query('select id, url from url_list')->hashes->to_array}) {
+      my $newurl = $rec->{url};
+      my $id = $rec->{id};
 
-    my $res = $ua->get($uaurl)->result;
-    # say $res->{message};
-    # say $res->{code};
-    say "@{$res}{qw/ code message /}";
-    # say $res->dom->at('title')->text;
-    # say Dumper($res->headers);
-    if ($res->headers->location){
-    say $res->headers->location;}
-    else {say ""};
-    say $res->headers->content_type;
-    say $res->headers->server;
-    # say $res->headers;
-  #  };
-  };    
+      my $ts = localtime(time);
+    
+      my $res_ora = $ua->get($newurl);
+      my ($hst, $hhl, $hhc, $hhs) = (" ", " ", " ", " ");
+
+      if (defined ($res_ora->original_remote_address)) {
+        my $res = $ua->get($newurl)->result;
+        $hst = $res->{code}." ".$res->{message};
+        $hhl = $res->headers->location;
+        $hhc =  $res->headers->content_type;
+        $hhs =  $res->headers->server;
+      }
+      else {
+        $hst = "Cannot resolve host";
+        $hhl = "Cannot resolve host";
+        $hhc = "Cannot resolve host";
+        $hhs = "Cannot resolve host";
+      };
+        
+      $self->pg->db->query('update url_list set (url, urldate, httpstatus, httphead1, httphead2, httphead3) = (?,?,?,?,?,?) where id = ?', $newurl, $ts, $hst, $hhl, $hhc, $hhs, $id);
+    };
 
     $self->redirect_to("/"); 
+    
 }
-
 
 1;
